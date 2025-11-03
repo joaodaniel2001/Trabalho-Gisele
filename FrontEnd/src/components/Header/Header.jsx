4f71ffc2a618970a80b08679bbfb88895dd77b6a
João <joaodaniel2al@gmail.com>
2025-11-03 12:18:11 -0300

    Icones no Header

--- FrontEnd/src/components/Header/Header.jsx
+++ FrontEnd/src/components/Header/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './Header.css'
 
+/* Ícones */
+import { IoHomeSharp } from "react-icons/io5";
+import { RiDashboardFill } from "react-icons/ri";
+import { GrTask } from "react-icons/gr";
+
 const Header = ({ menu }) => {
+
+    const headerIcons = [
+        { label: 'Home', icon: <IoHomeSharp /> },
+        { label: 'Dashboard', icon: <RiDashboardFill /> },
+        { label: 'Questionario', icon: <GrTask /> }
+    ]
+
+    const currentItem = headerIcons.find((item) => item.label === menu);
+
     return (
         <div className='header'>
-            <h1>{menu}</h1>
+            <h1>
+                {currentItem && currentItem.icon}
+                <span>{menu}</span>
+            </h1>
         </div>
     )
 }
